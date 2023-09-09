a3642011c5c99e9d0ec5fcd4b60ffd0bf6599d72
tommy <50880262+tiamygarden@users.noreply.github.com>
2023-09-09 17:45:59 +0200

    wip

--- MonCV/src/data/libraries.ts
+++ MonCV/src/data/libraries.ts
@@ -3,30 +3,30 @@ export const libraries = [
     "id": 1,
     "name": "Bootstrap",
     "stars": 3,
-    "logoPath": "src/assets/bootstrap.svg"
+    "logoPath": "/assets/bootstrap.svg"
   },
   {
     "id": 2,
     "name": "jQuery",
     "stars": 3,
-    "logoPath": "src/assets/jquery.svg"
+    "logoPath": "/assets/jquery.svg"
   },
   {
     "id": 3,
     "name": "React",
     "stars": 4,
-    "logoPath": "src/assets/react.svg"
+    "logoPath": "/assets/react.svg"
   },
   {
     "id": 4,
     "name": "Sass",
     "stars": 3,
-    "logoPath": "src/assets/sass.svg"
+    "logoPath": "/assets/sass.svg"
   },
   {
     "id": 5,
     "name": "Figma",
     "stars": 5,
-    "logoPath": "src/assets/figma.svg"
+    "logoPath": "/assets/figma.svg"
   }
 ] as const
