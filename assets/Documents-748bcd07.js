0ac51767d50081abca52c633b627b317345b8bb0
tommy <50880262+tiamygarden@users.noreply.github.com>
2023-09-29 18:58:32 +0200

    Updates

--- assets/Documents-748bcd07.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-3d27a455.js";import{M as t,a,D as r}from"./MainLayout-6bd6c939.js";const o=()=>s.jsxs(s.Fragment,{children:[s.jsx(t,{}),s.jsx(a,{children:s.jsx(r,{})})]});export{o as default};
