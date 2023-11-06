f19fdc060ffe6214c096aab1e9c071064487c6df
tommy <50880262+tiamygarden@users.noreply.github.com>
2023-11-07 00:15:15 +0100

    Updates

---
+++ assets/useDispatch-7bdafdaf.js
@@ -0,0 +1,2 @@
+import{o as e,p as u,q as c}from"./index-27156d17.js";function r(t=e){const o=t===e?u:c(t);return function(){const{store:s}=o();return s}}const a=r();function i(t=e){const o=t===e?a:r(t);return function(){return o().dispatch}}const x=i();export{x as u};
+//# sourceMappingURL=useDispatch-7bdafdaf.js.map
