1ff2b62c21cdfd22a94cef26024992d2670da369
tommy <50880262+tiamygarden@users.noreply.github.com>
2023-11-07 01:03:20 +0100

    Updates

--- assets/Portfolio-08a35587.js
+++
@@ -1,2 +0,0 @@
-import{j as e,P as s}from"./index-27156d17.js";import{M as t,a as o}from"./MainLayout-e26d2b28.js";const r=()=>e.jsxs("div",{className:"flex flex-col justify-center items-center",children:[e.jsx("h1",{className:"text-6xl font-bold text-gray-900 mb-5",children:"Welcome to My portfolio"}),e.jsx("div",{className:"container flex flex-wrap justify-around gap-6 py-4",children:e.jsx(s,{})})]}),i=()=>e.jsxs(e.Fragment,{children:[e.jsx(t,{}),e.jsx(o,{children:e.jsx(r,{})})]});export{i as default};
-//# sourceMappingURL=Portfolio-08a35587.js.map
