27b35117552cd1504ee6dfd2bd3438dccdfff4e5
@tommyRequillard <tommy.requillard@gmail.com>
2024-10-23 22:00:08 +0200

    mergedGitlabAndGithub

---
+++ push_script.ps1
@@ -0,0 +1,34 @@
+# Définir les variables pour les remotes
+$GITHUB_REMOTE = "github"
+$GITLAB_REMOTE = "gitlab"
+$LOG_FILE = "push_log.txt"
+
+# Date et heure pour le log
+Add-Content -Path $LOG_FILE -Value ("{0} {1} - Démarrage du push" -f (Get-Date).ToShortDateString(), (Get-Date).ToLongTimeString())
+
+# Ajouter tous les fichiers et effectuer un commit
+git add .
+
+# Demander à l'utilisateur d'entrer un message de commit
+$COMMIT_MESSAGE = Read-Host -Prompt "Entrez le message de commit"
+
+# Effectuer le commit avec le message fourni par l'utilisateur
+git commit -m "$COMMIT_MESSAGE"
+
+# Pousser vers GitHub
+git push $GITHUB_REMOTE master
+if ($LASTEXITCODE -eq 0) {
+    Add-Content -Path $LOG_FILE -Value "Push réussi vers GitHub"
+} else {
+    Add-Content -Path $LOG_FILE -Value "Erreur lors du push vers GitHub"
+}
+
+# Pousser vers GitLab
+git push $GITLAB_REMOTE master
+if ($LASTEXITCODE -eq 0) {
+    Add-Content -Path $LOG_FILE -Value "Push réussi vers GitLab"
+} else {
+    Add-Content -Path $LOG_FILE -Value "Erreur lors du push vers GitLab"
+}
+
+Add-Content -Path $LOG_FILE -Value ("{0} {1} - Fin du push" -f (Get-Date).ToShortDateString(), (Get-Date).ToLongTimeString())
--- src/components/cv/Card.tsx
+++ src/components/cv/Card.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import GaugeLine from '../GaugeLine'
 
 type CardTypes = {
@@ -20,26 +19,3 @@ const Card = ({name, stars, logoPath}: CardTypes) => {
 }
 
 export default Card
-=======
-import GaugeLine from '../GaugeLine'
-
-type CardTypes = {
-    name: string,
-    stars: number,
-    logoPath: string
-}
-
-const Card = ({name, stars, logoPath}: CardTypes) => {
-  return (
-    <div className="flex flex-col justify-center items-center h-10 mt-5">
-      <div className="flex flex-col justify-center items-center h-12">
-        <img src={logoPath} alt={name} className="w-10 h-6"/>
-      </div>
-      <div className="flex justify-center items-center gap-2 my-1">{name}</div>
-      <GaugeLine filled={stars} total={5}/>
-    </div>
-  )
-}
-
-export default Card
->>>>>>> 23f87d87311a44c6bba68fbde059e75bef16c8d7
