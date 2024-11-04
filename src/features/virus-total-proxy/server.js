4690809f06f4324898a840303122ee6f76b078e3
@tommyRequillard <tommy.requillard@gmail.com>
2024-11-05 00:13:22 +0100

    wip

--- src/features/virus-total-proxy/server.js
+++ src/features/virus-total-proxy/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
+import { body, validationResult } from 'express-validator';
 
 dotenv.config();
 
@@ -8,8 +9,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.disable("x-powered-by"); // Désactiver l'en-tête x-powered-by
 
-app.post('/api/scan', async (req, res) => {
+app.post('/api/scan', [
+    body('url').isURL().withMessage('URL must be valid'), // Validation de l'URL
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     try {
         const cleanUrl = req.body.url.replace(/^https?:\/\//, "");
         const response = await axios.post('https://www.virustotal.com/api/v3/urls', `url=${encodeURIComponent(cleanUrl)}`, {
@@ -20,11 +30,12 @@ app.post('/api/scan', async (req, res) => {
         });
         res.json(response.data);
     } catch (error) {
+        console.error('Error:', error.response ? error.response.data : error.message);
         res.status(error.response ? error.response.status : 500).json(error.response ? error.response.data : { message: "Erreur inconnue" });
     }
 });
 
-// dans un terminal saisir node server.js sur le path de ce folder
+// Démarrer le serveur
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
